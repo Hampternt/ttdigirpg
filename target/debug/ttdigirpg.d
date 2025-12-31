322ab01f347a9adbace05cd1eaa6badc97e64e65
Hampternt <jesperlovland@proton.me>
2025-12-31 03:39:20 +0100

    build: Update generated debug dependency file path

--- target/debug/ttdigirpg.d
+++ target/debug/ttdigirpg.d
@@ -1,1 +1,1 @@
-/home/hampter/ttdigirpg/target/debug/ttdigirpg: /home/hampter/ttdigirpg/src/main.rs
+/home/hampter/projects/ttdigirpg/target/debug/ttdigirpg: /home/hampter/projects/ttdigirpg/src/main.rs
